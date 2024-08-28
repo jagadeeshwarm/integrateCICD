fbc8c07accf88df08cb8a4c06c4d9acd6b048915
jagadeeshwarm <malagamjagdesh@gmail.com>
2024-08-28 13:48:26 +0530

    first commit in the ci/cd

---
+++ src/test/java/com/testgit/TestGit.java
@@ -0,0 +1,39 @@
+package com.testgit;
+
+public class TestGit {
+
+	public void gitmethod1() {
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+		System.out.println("test4");
+	}
+	
+	public void gitmethod2() {
+		System.out.println("git1");
+		System.out.println("git2");
+		System.out.println("git3");
+		System.out.println("git4");
+	}
+	
+	public void gitmethod3() {
+		System.out.println("AUT1");
+		System.out.println("AUT2");
+		System.out.println("AUT3");
+		System.out.println("AUT4");
+	}
+	
+	public void gitmethod4() {
+		System.out.println("git1");
+		System.out.println("git2");
+		System.out.println("git3");
+		System.out.println("git4");
+	}
+	
+	public void gitmethod5() {
+		System.out.println("git1");
+		System.out.println("git2");
+		System.out.println("git3");
+		System.out.println("git4");
+	}
+}
